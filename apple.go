8b55557dbdca2194014f38e3240591d1ff7477f3
Ian Wodder <ian.wodder@gmail.com>
2024-04-07 15:05:21 -0600

    Update apple(s) to move if eaten

--- apple.go
+++ apple.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"github.com/gdamore/tcell/v2"
+	"math/rand"
+)
 
 type apples []apple
 
@@ -10,11 +13,32 @@ func (as apples) draw(scn tcell.Screen) {
 	}
 }
 
+func (as apples) move() {
+	for i := range as {
+		as[i].move()
+	}
+}
+
 type apple struct {
 	pos   pos
 	eaten bool
 }
 
-func (a apple) draw(scn tcell.Screen) {
+func (a *apple) draw(scn tcell.Screen) {
 	scn.SetContent(a.pos.x, a.pos.y, 'A', nil, tcell.StyleDefault)
+}
+
+func (a *apple) move() {
+	if a.eaten {
+		p := pos{x: rand.Int(), y: rand.Int()}
+		for i := 0; a.pos == p; i++ {
+			if i%2 == 0 {
+				p.x = rand.Int()
+			} else {
+				p.y = rand.Int()
+			}
+		}
+		a.pos = p
+		a.eaten = false
+	}
 }
--- apple_test.go
+++ apple_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -30,4 +31,36 @@ func Test_CanDrawApples(t *testing.T) {
 	requireEqualContents(t, 1, 1, 'A', scn)
 	requireEqualContents(t, 2, 2, 'A', scn)
 	requireEqualContents(t, 3, 3, 'A', scn)
+}
+
+func Test_IfAppleIsEatenThenPositionIsUpdatedAndItsNotEaten(t *testing.T) {
+	a := apple{pos: pos{x: 10, y: 10}, eaten: true}
+
+	a.move()
+
+	require.NotEqual(t, pos{x: 10, y: 10}, a.pos)
+	require.False(t, a.eaten)
+}
+
+func Test_IfAppleIsNotEatenThenPositionDoesNotChange(t *testing.T) {
+	a := apple{pos: pos{x: 10, y: 10}, eaten: false}
+
+	a.move()
+
+	require.Equal(t, pos{x: 10, y: 10}, a.pos)
+	require.False(t, a.eaten)
+}
+
+func Test_CanMoveApples(t *testing.T) {
+	a := apples{
+		{pos: pos{x: 1, y: 1}, eaten: true},
+		{pos: pos{x: 2, y: 2}, eaten: false},
+		{pos: pos{x: 3, y: 3}, eaten: true},
+	}
+
+	a.move()
+
+	require.NotEqual(t, apple{pos: pos{x: 1, y: 1}, eaten: true}, a[0])
+	require.Equal(t, apple{pos: pos{x: 2, y: 2}, eaten: false}, a[1])
+	require.NotEqual(t, apple{pos: pos{x: 3, y: 3}, eaten: true}, a[0])
 }
