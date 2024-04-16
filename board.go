93e42aee73743a7f59e6252fecbfde4938f91f93
Ian Wodder <ian.wodder@gmail.com>
2024-04-16 16:36:27 -0600

    Fill game board with a solid color

--- board.go
+++ board.go
@@ -2,38 +2,47 @@ package main
 
 import "github.com/gdamore/tcell/v2"
 
-type border struct {
+type board struct {
 	height, width int
 	style         tcell.Style
 }
 
-func (b *border) draw(scn tcell.Screen) {
+func (b *board) draw(scn tcell.Screen) {
+	b.fill(scn)
 	b.drawHorizontalEdges(scn)
 	b.drawVerticalEdges(scn)
 	b.setCorners(scn)
 }
 
-func (b *border) drawHorizontalEdges(scn tcell.Screen) {
+func (b *board) fill(scn tcell.Screen) {
+	for x := range b.width {
+		for y := range b.height {
+			scn.SetContent(x, y, ' ', nil, b.style)
+		}
+	}
+}
+
+func (b *board) drawHorizontalEdges(scn tcell.Screen) {
 	for x := range b.width {
 		scn.SetContent(x, 0, tcell.RuneHLine, nil, b.style)
 		scn.SetContent(x, b.height-1, tcell.RuneHLine, nil, b.style)
 	}
 }
 
-func (b *border) drawVerticalEdges(scn tcell.Screen) {
+func (b *board) drawVerticalEdges(scn tcell.Screen) {
 	for y := range b.height {
 		scn.SetContent(0, y, tcell.RuneVLine, nil, b.style)
 		scn.SetContent(b.width-1, y, tcell.RuneVLine, nil, b.style)
 	}
 }
 
-func (b *border) setCorners(scn tcell.Screen) {
+func (b *board) setCorners(scn tcell.Screen) {
 	scn.SetContent(0, 0, tcell.RuneULCorner, nil, b.style)
 	scn.SetContent(b.width-1, 0, tcell.RuneURCorner, nil, b.style)
 	scn.SetContent(0, b.height-1, tcell.RuneLLCorner, nil, b.style)
 	scn.SetContent(b.width-1, b.height-1, tcell.RuneLRCorner, nil, b.style)
 }
 
-func newBorder(height, width int) *border {
-	return &border{height: height, width: width, style: tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)}
+func newBoard(height, width int) *board {
+	return &board{height: height, width: width, style: tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)}
 }
--- board_test.go
+++ board_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_BoxCanDraw(t *testing.T) {
 	dst := setupScreen(t, 10, 10)
 
-	b := newBorder(10, 10)
+	b := newBoard(10, 10)
 
 	b.draw(dst)
 
@@ -26,4 +27,19 @@ func Test_BoxCanDraw(t *testing.T) {
 	}
 
 	requireEqualScreen(t, exp[:], dst)
+}
+
+func Test_BoxFillsEachCell(t *testing.T) {
+	dst := setupScreen(t, 10, 10)
+
+	b := newBoard(10, 10)
+
+	b.draw(dst)
+
+	for x := range 10 {
+		for y := range 10 {
+			_, _, style, _ := dst.GetContent(x, y)
+			assert.Equal(t, b.style, style, "cell (x=%d,y=%d) didn't have the correct style", x, y)
+		}
+	}
 }
